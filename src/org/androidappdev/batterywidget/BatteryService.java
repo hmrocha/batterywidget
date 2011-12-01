87347304a5bc714ffb31a440bb5540d43c64b2c2
Henrique Rocha <hmrocha@gmail.com>
2011-12-01 12:45:52 +0000

    Implemented a feature request: by Richard (November 7, 2011) It would be great if you could bring up the battery usage screen when I click on the widget. At the moment, nothing happens when I click the widget and that seems like a waste!

--- src/org/androidappdev/batterywidget/BatteryService.java
+++ src/org/androidappdev/batterywidget/BatteryService.java
@@ -17,6 +17,7 @@
  */
 package org.androidappdev.batterywidget;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
@@ -36,131 +37,132 @@ import android.widget.RemoteViews;
  * @author Henrique Rocha
  */
 public class BatteryService extends Service {
-	private static final String TAG = "BatteryService";
-	private BroadcastReceiver batteryReceiver;
-	private Integer currentLevel = 0;
-	private Integer currentTemperature = 0;
-	private int previousStatus = -1;
+    private static final String TAG = "BatteryService";
+    private BroadcastReceiver batteryReceiver;
+    private Integer currentLevel = 0;
+    private Integer currentTemperature = 0;
+    private int previousStatus = -1;
 
-	@Override
-	public void onStart(Intent intent, int startId) {
-		if (this.batteryReceiver == null) {
-			this.batteryReceiver = batteryLevelReceiver();
-			registerReceiver(this.batteryReceiver, new IntentFilter(
-					Intent.ACTION_BATTERY_CHANGED));
-		}
+    @Override
+    public void onStart(Intent intent, int startId) {
+        if (this.batteryReceiver == null) {
+            this.batteryReceiver = batteryLevelReceiver();
+            registerReceiver(this.batteryReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        }
 
-		// Build the widget update.
-		RemoteViews updateViews = buildUpdate(this);
+        // Build the widget update.
+        RemoteViews updateViews = buildUpdate(this);
+        
+        // Show power usage when widget is tapped.
+        Intent intentBatteryUsage = new Intent(Intent.ACTION_POWER_USAGE_SUMMARY);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intentBatteryUsage,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        updateViews.setOnClickPendingIntent(R.id.widget, pendingIntent);
+        
+        // Push update for this widget to the home screen
+        ComponentName batteryWidget = new ComponentName(this, BatteryAppWidgetProvider.class);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        appWidgetManager.updateAppWidget(batteryWidget, updateViews);
+    }
 
-		// Push update for this widget to the home screen
-		ComponentName batteryWidget = new ComponentName(this,
-				BatteryAppWidgetProvider.class);
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-		appWidgetManager.updateAppWidget(batteryWidget, updateViews);
-	}
+    /**
+     * Battery level receiver
+     * 
+     * @return a BroadcastReceiver to handle ACTION_BATTERY_CHANGED
+     */
+    private BroadcastReceiver batteryLevelReceiver() {
+        return new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                boolean needsUpdate = false;
+                int layout = -1;
+                String action = intent.getAction();
 
-	/**
-	 * Battery level receiver
-	 * 
-	 * @return a BroadcastReceiver to handle ACTION_BATTERY_CHANGED
-	 */
-	private BroadcastReceiver batteryLevelReceiver() {
-		return new BroadcastReceiver() {
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				boolean needsUpdate = false;
-				int layout = -1;
-				String action = intent.getAction();
+                // Change dots to blue if charging.
+                int status = intent.getIntExtra("status", 0);
+                Log.d(TAG, "status: " + status);
 
-				// Change dots to blue if charging.
-				int status = intent.getIntExtra("status", 0);
-				Log.d(TAG, "status: " + status);
+                if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                    int level = intent.getIntExtra("level", 0);
+                    int temperature = intent.getIntExtra("temperature", 0);
+                    Log.d(TAG, "Level: " + level);
+                    // Only update widget if level changed, other changes
+                    // like temperature don't matter.
+                    if (currentLevel != level) {
+                        currentLevel = level;
+                        needsUpdate = true;
+                    }
+                    boolean showTemperature = BatteryWidgetConfigure.getTemperaturePref(context);
+                    if (showTemperature && currentTemperature != temperature) {
+                        currentTemperature = temperature;
+                        needsUpdate = true;
+                    }
 
-				if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
-					int level = intent.getIntExtra("level", 0);
-					int temperature = intent.getIntExtra("temperature", 0);
-					Log.d(TAG, "Level: " + level);
-					// Only update widget if level changed, other changes
-					// like temperature don't matter.
-					if (currentLevel != level) {
-						currentLevel = level;						
-						needsUpdate = true;
-					}
-					boolean showTemperature = BatteryWidgetConfigure.getTemperaturePref(context);
-					if (showTemperature && currentTemperature != temperature) {
-						currentTemperature = temperature;
-						needsUpdate = true;
-					}
+                    if (level <= 100)
+                        layout = R.layout.main;
+                    if (level <= 30)
+                        layout = R.layout.below30;
+                    if (level <= 15)
+                        layout = R.layout.below15;
 
-					if (level <= 100)
-						layout = R.layout.main;
-					if (level <= 30)
-						layout = R.layout.below30;
-					if (level <= 15)
-						layout = R.layout.below15;
+                }
 
-				}
+                if (status == BatteryManager.BATTERY_STATUS_CHARGING
+                        && previousStatus != BatteryManager.BATTERY_STATUS_CHARGING) {
+                    BatteryAppWidgetProvider.currentLayout = R.layout.charging;
+                    needsUpdate = true;
+                } else if (status == BatteryManager.BATTERY_STATUS_DISCHARGING
+                        && previousStatus != BatteryManager.BATTERY_STATUS_DISCHARGING) {
+                    BatteryAppWidgetProvider.currentLayout = layout;
+                    needsUpdate = true;
+                }
 
-				if (status == BatteryManager.BATTERY_STATUS_CHARGING
-						&& previousStatus != BatteryManager.BATTERY_STATUS_CHARGING) {
-					BatteryAppWidgetProvider.currentLayout = R.layout.charging;
-					needsUpdate = true;
-				} else if (status == BatteryManager.BATTERY_STATUS_DISCHARGING
-						&& previousStatus != BatteryManager.BATTERY_STATUS_DISCHARGING) {
-					BatteryAppWidgetProvider.currentLayout = layout;
-					needsUpdate = true;
-				}
+                if (needsUpdate) {
+                    Intent statusChanged = new Intent(context, BatteryService.class);
+                    context.startService(statusChanged);
+                }
+            }
+        };
+    }
 
-				if (needsUpdate) {
-					Intent statusChanged = new Intent(context,
-							BatteryService.class);
-					context.startService(statusChanged);
-				}
-			}
-		};
-	}
+    /**
+     * Build a widget update to show the current battery level.
+     */
+    private RemoteViews buildUpdate(Context context) {
+        RemoteViews views = new RemoteViews(context.getPackageName(),
+                BatteryAppWidgetProvider.currentLayout);
 
-	/**
-	 * Build a widget update to show the current battery level.
-	 */
-	private RemoteViews buildUpdate(Context context) {
-		RemoteViews views = new RemoteViews(context.getPackageName(),
-				BatteryAppWidgetProvider.currentLayout);
+        // Action for tap on widget
+        // Intent bcast = new Intent(context, BatteryAppWidgetProvider.class);
+        // bcast.setAction(BatteryAppWidgetProvider.ACTION_CHANGE_BG);
+        // PendingIntent pending = PendingIntent.getBroadcast(context, 0, bcast,
+        // PendingIntent.FLAG_UPDATE_CURRENT);
+        // views.setOnClickPendingIntent(R.id.widget, pending);
 
-		// Action for tap on widget
-		// Intent bcast = new Intent(context, BatteryAppWidgetProvider.class);
-		// bcast.setAction(BatteryAppWidgetProvider.ACTION_CHANGE_BG);
-		// PendingIntent pending = PendingIntent.getBroadcast(context, 0, bcast,
-		// PendingIntent.FLAG_UPDATE_CURRENT);
-		// views.setOnClickPendingIntent(R.id.widget, pending);
+        views.setTextViewText(R.id.battery_level, this.currentLevel.toString());
+        Integer temperature = this.currentTemperature / 10;
 
-		views.setTextViewText(R.id.battery_level, this.currentLevel.toString());
-		Integer temperature = this.currentTemperature / 10;
-		
-		boolean showTemperature = BatteryWidgetConfigure.getTemperaturePref(context);
-		if (showTemperature) {
-			int degrees = BatteryWidgetConfigure.getDegreesPref(context);
-			Log.d(TAG, "degrees:" + degrees);
-			if (degrees == BatteryWidgetConfigure.PREF_FAHRENHEIT) {
-				 temperature = (int) (1.8 * (temperature + 32) + 0.5);
-			}
-			views.setTextViewText(R.id.temperature, temperature + "º");				
-			views.setViewVisibility(R.id.temperature, View.VISIBLE);
-		}
-		else {
-			views.setViewVisibility(R.id.temperature, View.GONE);			
-		}
-		ComponentName cn = new ComponentName(context,
-				BatteryAppWidgetProvider.class);
-		AppWidgetManager.getInstance(context).updateAppWidget(cn, views);
-		return views;
-	}
+        boolean showTemperature = BatteryWidgetConfigure.getTemperaturePref(context);
+        if (showTemperature) {
+            int degrees = BatteryWidgetConfigure.getDegreesPref(context);
+            Log.d(TAG, "degrees:" + degrees);
+            if (degrees == BatteryWidgetConfigure.PREF_FAHRENHEIT) {
+                temperature = (int) (1.8 * (temperature + 32) + 0.5);
+            }
+            views.setTextViewText(R.id.temperature, temperature + "º");
+            views.setViewVisibility(R.id.temperature, View.VISIBLE);
+        } else {
+            views.setViewVisibility(R.id.temperature, View.GONE);
+        }
+        ComponentName cn = new ComponentName(context, BatteryAppWidgetProvider.class);
+        AppWidgetManager.getInstance(context).updateAppWidget(cn, views);
+        return views;
+    }
 
-	@Override
-	public IBinder onBind(Intent arg0) {
-		// We don't need to bind to this service
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent arg0) {
+        // We don't need to bind to this service
+        return null;
+    }
 
 }
